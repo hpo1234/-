58d9dab5aef8c5e759354443c79ca5166524f029
hpo1234 <105036178+hpo1234@users.noreply.github.com>
2022-06-28 10:18:15 +0800

    Add files via upload

---
+++ chess源码/src/中国象棋2/象棋ai.java
@@ -0,0 +1,11 @@
+package 中国象棋2;
+
+public class 象棋ai {
+
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+}
---
+++ chess源码/src/象棋1/棋盘.java
@@ -0,0 +1,47 @@
+package œÛ∆Â1;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+/**
+* @Description 
+* @author ◊£∏£
+* 2021-06-24
+*/
+public class ∆Â≈Ã extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					∆Â≈Ã frame = new ∆Â≈Ã();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ∆Â≈Ã() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
